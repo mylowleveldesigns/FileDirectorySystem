b7916f6710e0a40f10bf1776a95e661874f06351
ummalhotra <umangmalhotra1995@gmail.com>
2024-06-01 20:28:16 +0530

    Enum for all commands

--- src/main/java/com/umang/lld/fds/commands/CommandFinder.java
+++ src/main/java/com/umang/lld/fds/commands/CommandFinder.java
@@ -2,50 +2,75 @@ package com.umang.lld.fds.commands;
 
 public class CommandFinder {
 
-    static String SEPERATOR = "/";
+    static String SEPARATOR = "/";
     static String SPACE = " ";
 
-    private interface allCommands {
-        String LS = "ls";
-        String PWD = "pwd";
-        String MKDIR = "mkdir";
-        String CD = "cd";
-        String RM = "rm";
-        String SESSION_CLEAR = "session clear";
+    public enum CommandType {
+        LS("ls"),
+        PWD("pwd"),
+        MKDIR("mkdir"),
+        CD("cd"),
+        RM("rm"),
+        SESSION_CLEAR("session clear");
+
+        private final String command;
+
+        CommandType(String command) {
+            this.command = command;
+        }
+
+        public String getCommand() {
+            return command;
+        }
+
+        public static CommandType fromString(String command) {
+            for (CommandType type : CommandType.values()) {
+                if (type.getCommand().equals(command)) {
+                    return type;
+                }
+            }
+            return null;
+        }
     }
 
     public static Command findFromString(String input) {
         if (input == null || input.equals("")) {
             return new UnknownCommand();
         }
-        String[] tokens = input.split(" ");
+        String[] tokens = input.split(SPACE);
         if (tokens.length < 1) {
             return new UnknownCommand();
         }
         String commandName = tokens[0];
-        switch (commandName) {
-            case allCommands.LS:
+        CommandType commandType = CommandType.fromString(commandName);
+        if (commandType == null) {
+            return new UnknownCommand();
+        }
+        switch (commandType) {
+            case LS:
                 if (tokens.length == 1) {
                     return new Ls();
                 } else return new InvalidCommand();
-            case allCommands.PWD:
-                 return new Pwd();
-            case allCommands.MKDIR:
+            case PWD:
+                return new Pwd();
+            case MKDIR:
                 if (tokens.length != 2) {
                     return new InvalidCommand();
                 } else return new Mkdir(tokens[1]);
-            case allCommands.CD:
+            case CD:
                 if (tokens.length != 2) {
                     return new InvalidCommand();
-                } else return new Cd (tokens[1]);
-            case allCommands.RM:
+                } else return new Cd(tokens[1]);
+            case RM:
                 if (tokens.length != 2) {
                     return new InvalidCommand();
                 } else return new Rm(tokens[2]);
-            default:
-                if(allCommands.SESSION_CLEAR.equals(input)){
+            case SESSION_CLEAR:
+                if (input.equals(CommandType.SESSION_CLEAR.getCommand())) {
                     return new SessionClear();
                 }
+                return new UnknownCommand();
+            default:
                 return new UnknownCommand();
         }
     }
